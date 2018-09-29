c8f359144b0f1b6f43c0915f87c9f7c8b973d225
Peter Dolan <dolan118@morris.umn.edu>
2018-09-29 18:59:57 -0500

    reconciling...

--- src/DataGenerator2018.java
+++ src/DataGenerator2018.java
@@ -1,78 +1,72 @@
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Random;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class DataGenerator2018 {
-	/**
-	 * See Readme for an overview of the data generating process. The result is
-	 * being written to the file given as the first command line argument.
-	 * 
-	 * @param args
-	 * 
-	 * 
-	 * 
-	 *            Author: Peter Dolan (based off work by Elena Machkasova)
-	 */
+    /**
+     * See Readme for an overview of the data generating process. The result is
+     * being written to the file given as the first command line argument.
+     *
+     * @param args
+     *
+     *
+     *
+     *            Author: Peter Dolan (based off work by Elena Machkasova)
+     */
 
-	// relative probabilities of each length up to maxLength; add up to 1500:
-	private static Random rand = new Random();
-	private static corpus=new String(Files.readAllByetes(Paths.get("coprus.txt"))); //I've hardwired in the corpus.txt requirement... bad Peter!
+    // relative probabilities of each length up to maxLength; add up to 1500:
+    private static Random rand = new Random();
+    private static String corpus;
 
-	public static void main(String[] args) throws FileNotFoundException {
+    public static void main(String[] args) throws FileNotFoundException,IOException {
 
-		String filename = "nofile";
-		PrintWriter out = null;
-		// Reading main args
-		
-		// if there is a file specified, open a PrintWriter
-		if (args.length >= 1) {
-			filename = args[0];			
-			out = new PrintWriter(filename);
-		}
+            
+    	corpus=new String(Files.readAllBytes(Paths.get("corpus.txt"))); //I've hardwired in the corpus.txt requirement... bad Peter!
+    	String filename = "nofile";
+            PrintWriter out = null;
+            // Reading main args
 
-		generateData(10000, out);
-		
-		if (out != null) {
-			out.close();
-		}
-	
-	}
-	
-	
-	// Methods for generating the test data: 
-	
-	private int generateLengths(int n) {
-		this.lengths=new int[n];
-		for (int i = 0; i < n; ++i) { 
-			this.lengths[i]=100+rand.nextInt(900); //Random int between 100 and 1000
-		}
-		this.size=n;	
-		return maxLength; // a.k.a. thresholds.length + 1
-	}
-	
-	// generate data and print it to a file:
-	
-	private static void generateData(int numElts, PrintWriter out) {
-		
-		// numbers are generated digit-by-digit.
-		// An array to store the digits:
-		int start;
-		int length;
-		String sample;
+            // if there is a file specified, open a PrintWriter
+            if (args.length >= 1) {
+                    filename = args[0];
+                    out = new PrintWriter(filename);
+            }
 
-		for (int i = 0; i < numElts; ++i) {
-			// generate length:
-			length = 100+rand.nextInt(900) // Random int between 100 and 1000
-			start = 1+rand.nextInt(corpus.size-length);
-			sample=this.corpus.substring(start,start+corpus.length);
+            generateData(10000, out);
 
-			if (out == null) { // no file specified, so print to standard output
-				System.out.println(sample);
-			} else { // print to the print writer:
-				out.println(sample);
-				}
-			}
-		}
-	}
-	
-}
+            if (out != null) {
+                    out.close();
+            }
+
+    }
+
+
+
+    // generate data and print it to a file:
+
+    private static void generateData(int numElts, PrintWriter out) {
+
+            // numbers are generated digit-by-digit.
+            // An array to store the digits:
+            int start;
+            int length;
+            String sample;
+
+            for (int i = 0; i < numElts; ++i) {
+                    // generate length:
+                    length = 100+rand.nextInt(900); // Random int between 100 and 1000
+                    start = 1+rand.nextInt(corpus.length()-length);
+                 
+                    sample=corpus.substring(start,start+length);
+
+                    if (out == null) { // no file specified, so print to standard output
+                            System.out.println(sample);
+                    } else { // print to the print writer:
+                            out.println(sample);
+                            }
+                    }
+            }
+    }
