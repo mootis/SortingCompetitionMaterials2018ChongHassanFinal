dabc0a4cecf399054f9e12cc7f81ccd159d2985d
dolan118 <dolan118@morris.umn.edu>
2018-10-04 12:17:23 -0500

    Updated Generator from Eclipse code

--- src/DataGenerator2018.java
+++ src/DataGenerator2018.java
@@ -22,29 +22,23 @@ public class DataGenerator2018 {
     private static String corpus;
 
     public static void main(String[] args) throws FileNotFoundException,IOException {
-
-            
-    	corpus=new String(Files.readAllBytes(Paths.get("corpus.txt"))); //I've hardwired in the corpus.txt requirement... bad Peter!
-    	String filename = "nofile";
-            PrintWriter out = null;
-            // Reading main args
-
-            // if there is a file specified, open a PrintWriter
-            if (args.length >= 1) {
-                    filename = args[0];
-                    out = new PrintWriter(filename);
-            }
-
-            generateData(10000, out);
-
-            if (out != null) {
-                    out.close();
-            }
+        corpus=new String(Files.readAllBytes(Paths.get("corpus.txt"))); //I've hardwired in the corpus.txt requirement... bad Peter!
+        makeSet("data0.txt",10000);
+        makeSet("data1.txt",10000);
+        makeSet("data1.txt",10000);
 
     }
 
 
+    private static void makeSet(String outFileName, int n) throws FileNotFoundException,IOException {
+        PrintWriter out = new PrintWriter(outFileName);
+        generateData(n, out);
 
+        if (out != null) {
+                out.close();
+        }
+
+    }
     // generate data and print it to a file:
 
     private static void generateData(int numElts, PrintWriter out) {
@@ -57,9 +51,9 @@ public class DataGenerator2018 {
 
             for (int i = 0; i < numElts; ++i) {
                     // generate length:
-                    length = 100+rand.nextInt(900); // Random int between 100 and 1000
+                    length = 100+rand.nextInt(100); // Random int between 30 and 80
                     start = 1+rand.nextInt(corpus.length()-length);
-                 
+
                     sample=corpus.substring(start,start+length);
 
                     if (out == null) { // no file specified, so print to standard output
